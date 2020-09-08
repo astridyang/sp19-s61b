3d13b13f34f6fb4c21ac174d42832b77fa0bf5cc
sallyyou <workyooll@gmail.com>
2020-09-08 23:40:27 +0800

    finish proj0

--- proj0/Body.java
+++ proj0/Body.java
@@ -15,7 +15,8 @@ public class Body {
         mass = m;
         imgFileName = img;
     }
-    public Body(Body b){
+
+    public Body(Body b) {
         xxPos = b.xxPos;
         yyPos = b.yyPos;
         xxVel = b.xxVel;
@@ -23,12 +24,13 @@ public class Body {
         mass = b.mass;
         imgFileName = b.imgFileName;
     }
+
     /**
      * 
      * @param b
      * @return
      */
-    public double calcDistance(Body b){
+    public double calcDistance(Body b) {
         double dx = this.xxPos - b.xxPos;
         double dy = this.yyPos - b.yyPos;
         double r2 = Math.pow(dx, 2) + Math.pow(dy, 2);
@@ -36,7 +38,7 @@ public class Body {
         return r;
     }
 
-    public double calcForceExertedBy(Body b){
+    public double calcForceExertedBy(Body b) {
         double r = this.calcDistance(b);
         double F = (G * this.mass * b.mass) / Math.pow(r, 2);
         return F;
@@ -45,14 +47,53 @@ public class Body {
     public double calcForceExertedByX(Body b) {
         double F = this.calcForceExertedBy(b);
         double r = this.calcDistance(b);
-        double Fx = F * (b.xxPos - this.xxPos)/r;
+        double Fx = F * (b.xxPos - this.xxPos) / r;
         return Fx;
     }
+
     public double calcForceExertedByY(Body b) {
         double F = this.calcForceExertedBy(b);
         double r = this.calcDistance(b);
-        double Fy = F * (b.yyPos - this.yyPos)/r;
+        double Fy = F * (b.yyPos - this.yyPos) / r;
         return Fy;
     }
 
+    public double calcNetForceExertedByX(Body[] allBodies) {
+        double force = 0;
+        for (Body body : allBodies) {
+            if (body.equals(this)) {
+                continue;
+            }
+            force += this.calcForceExertedByX(body);
+        }
+        return force;
+    }
+
+    public double calcNetForceExertedByY(Body[] allBodies) {
+        double force = 0;
+        for (Body body : allBodies) {
+            if (body.equals(this)) {
+                continue;
+            }
+            force += this.calcForceExertedByY(body);
+        }
+        return force;
+    }
+
+    public void update(double dt, double fX, double fY) {
+        double aX = fX / this.mass;
+        double aY = fY / this.mass;
+        double vnewX = this.xxVel + dt * aX;
+        double vnewY = this.yyVel + dt * aY;
+        double pnewX = this.xxPos + dt * vnewX;
+        double pnewY = this.yyPos + dt * vnewY;
+        this.xxVel = vnewX;
+        this.yyVel = vnewY;
+        this.xxPos = pnewX;
+        this.yyPos = pnewY;
+    }
+
+    public void draw() {
+        StdDraw.picture(xxPos, yyPos, "./images/" + imgFileName);
+    }
 }
